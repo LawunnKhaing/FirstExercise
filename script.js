dc714f9deb687231d90ac35862b6eff993733bc7
Lawunn Khaing <lawunnkhaing.tgi@gmail.com>
2024-01-08 20:34:37 +0200

    Initial Commit

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
 document.getElementById("actionButton").addEventListener("click", function() {
-    alert("Button clicked!");
+    var description = document.getElementById("description");
+    var descriptionStyle = window.getComputedStyle(description);
+
+    if (descriptionStyle.display === "none" || descriptionStyle.display === "") {
+        description.style.display = "block";
+    } else {
+        description.style.display = "none";
+    }
 });
+
+
+document.getElementById("currentYear").textContent = new Date().getFullYear();
+
